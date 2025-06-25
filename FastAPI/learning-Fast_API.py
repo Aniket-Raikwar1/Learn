ef10a2e981cbbe02d458a1df84c57da1f06255e0
Aniket <aniketrtm114@gmail.com>
2025-06-25 13:26:53 +0530

    app

--- FastAPI/learning-Fast_API.py
+++ FastAPI/learning-Fast_API.py
@@ -1,1 +1,12 @@
-print("hi")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/about")
+def about():
+    return {"message": "I will be the first Trillionare in the world!"}
